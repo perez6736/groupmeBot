c86525a845f42bc5286416bb8c6c3029bc4e8ae6
perez6736 <perez6736@gmail.com>
2017-11-12 18:07:02 -0500

    testing google search

--- bot.js
+++ bot.js
@@ -16,14 +16,25 @@ var botID = process.env.BOTID;
 // maybe ask for the response as a parameter so we can do stuff on this js file instead of the server one. 
 function respond(res, requestBody){
 
-  console.log(requestBody.text.substring(0,1));
+  console.log(requestBody.text.substring(0,2));
 
   // if the user is not the bot do something 
   if(requestBody.user_id != "557959"){
     axiosMessage("Hello World.", res);
+
+    if(requestBody.text.substring(0,2) === ".g"){
+      googleURL(requestBody.text.substring(0,3));
+    }
   }
 
 }
+
+function googleURL(searchQuery){
+  var formattedQuery = searchQuery.split(' ').join('+');
+  console.log(searchQuery);
+}
+
+
 
 function axiosMessage(message, res){
   axios.post('https://api.groupme.com/v3/bots/post', {
@@ -31,11 +42,13 @@ function axiosMessage(message, res){
     "text"    : message
   })
   .then(function (response) {
-    res.json("post completed from app.")
-    console.log(response);
+    res.json("post completed from app.");
+    console.log("response from axios goes here");
+    //console.log(response);
   })
   .catch(function (error) {
-    console.log(error);
+    console.log("error will go here" );
+    //console.log(error);
   });
 }
   
