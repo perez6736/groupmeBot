533b66a5c1b0fcbc9c26330b1e6174154c0ca962
perez6736 <perez6736@gmail.com>
2017-11-19 15:14:42 -0500

    fixed axio messages.

--- bot.js
+++ bot.js
@@ -146,14 +146,14 @@ function threeDiceCiLo (){
   // check to see if a 456 was rolled. 
   if(sortedDiceRoll[0] === 4 && sortedDiceRoll[1] === 5 && sortedDiceRoll[2] === 6){
     console.log("you rolled a 456");
-    axiosMessage("4 5 6 .. You Win");
+    axiosMessage(diceRoll1 + " " + diceRoll2 + " " + diceRoll2 +  " - 4 5 6 .. You Win");
     return; 
   }
 
   // check to see if trips were rolled. 
   if(sortedDiceRoll[0] === sortedDiceRoll[1] && sortedDiceRoll[0] === sortedDiceRoll[2]){
     console.log("you rolled trips of " + sortedDiceRoll[0]); 
-    axiosMessage("You rolled trips of " + sortedDiceRoll[0]); 
+    axiosMessage(diceRoll1 + " " + diceRoll2 + " " + diceRoll2 + " - You rolled trips of " + sortedDiceRoll[0]); 
     return; 
   }
 
@@ -166,7 +166,7 @@ function threeDiceCiLo (){
         //then the match was 0 and 1 
         score = sortedDiceRoll[2]
         console.log("score was " + score);
-        axiosMessage("You got a score of " + score);
+        axiosMessage(diceRoll1 + " " + diceRoll2 + " " + diceRoll2 + " - You got a score of " + score);
         return;
 
       }
@@ -174,7 +174,7 @@ function threeDiceCiLo (){
         //then the mmatch was 1 and 2 
         score = sortedDiceRoll[0]
         console.log("score was " + score);
-        axiosMessage("You got a score of " + score);
+        axiosMessage(diceRoll1 + " " + diceRoll2 + " " + diceRoll2 + " - You got a score of " + score);
         return;
       }
 
@@ -182,7 +182,7 @@ function threeDiceCiLo (){
   }
 
   console.log(" no score roll again. ");
-  axiosMessage("No socre try tolling again." );
+  axiosMessage(diceRoll1 + " " + diceRoll2 + " " + diceRoll2 + " - No socre try tolling again." );
 
 }
 
