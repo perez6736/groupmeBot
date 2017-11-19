cd11f56d9d82ec84926d7c29463f637453bdd287
perez6736 <perez6736@gmail.com>
2017-11-19 15:12:46 -0500

    ci lo first draft

--- bot.js
+++ bot.js
@@ -132,6 +132,60 @@ function randomGif(gifTopic){
   
 }
 
+function threeDiceCiLo (){
+
+  var score;
+  //when some one says  /ci-lo we need to roll thress dice
+  var diceRoll = chance.rpg('3d6');
+  var diceRoll1 = diceRoll[0];
+  var diceRoll2 = diceRoll[1];
+  var diceRoll3 = diceRoll[2]; 
+  // sort the array first then check each index 
+  var sortedDiceRoll = diceRoll.sort(function(a, b){return a - b});
+
+  // check to see if a 456 was rolled. 
+  if(sortedDiceRoll[0] === 4 && sortedDiceRoll[1] === 5 && sortedDiceRoll[2] === 6){
+    console.log("you rolled a 456");
+    axiosMessage("4 5 6 .. You Win");
+    return; 
+  }
+
+  // check to see if trips were rolled. 
+  if(sortedDiceRoll[0] === sortedDiceRoll[1] && sortedDiceRoll[0] === sortedDiceRoll[2]){
+    console.log("you rolled trips of " + sortedDiceRoll[0]); 
+    axiosMessage("You rolled trips of " + sortedDiceRoll[0]); 
+    return; 
+  }
+
+  //  The most efficient way is to sort first, then find if any two adjacent elements are equal:
+  for (i=0; i<sortedDiceRoll.length){
+    if (sortedDiceRoll[i] === sortedDiceRoll[i+1]){
+      // there was a pair that wasnt trips -- need to see which index was not the match. 
+
+      if(i === 0){
+        //then the match was 0 and 1 
+        score = sortedDiceRoll[2]
+        console.log("score was " + score);
+        axiosMessage("You got a score of " + score);
+        return;
+
+      }
+      else if (i === 1){
+        //then the mmatch was 1 and 2 
+        score = sortedDiceRoll[0]
+        console.log("score was " + score);
+        axiosMessage("You got a score of " + score);
+        return;
+      }
+
+    }
+  }
+
+  console.log(" no score roll again. ");
+  axiosMessage("No socre try tolling again." );
+
+}
+
 
 // this will display all the commands the bot knows
 // add the ability to ask for help on certain commands. 
@@ -148,7 +202,6 @@ function help (){
 // - damon spell checking 
 // - reddit posts 
 // - send a text message from groupme 
-// - keep cool guy 
 
 
 //axios functions -- this makes the post request to group me to send the message and will add some to make ajax calls. 
