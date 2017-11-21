9764b40ea4daa39aec0db6126f9202e68521c41d
danny perez <perez6736@gmail.com>
2017-11-21 00:31:53 -0500

    added comments to code to make things clear.

--- bot.js
+++ bot.js
@@ -157,6 +157,7 @@ function randomGif(gifTopic){
   else{
     giphy.random(gifTopic, function (err,res){
       console.log(res);
+      // sometimes we wont get a gif back -- thats just life. 
       if(res.data.length === 0){
         axiosMessage("Sorry. No gifs matching -" + gifTopic);
       }
@@ -173,12 +174,12 @@ function randomGif(gifTopic){
 function threeDiceCiLo (){
 
   var score;
-  //when some one says  /ci-lo we need to roll three dice
+  //when some one says  /ci-lo we need to roll three six sided dice
   var diceRoll = chance.rpg('3d6');
   var diceRoll1 = diceRoll[0];
   var diceRoll2 = diceRoll[1];
   var diceRoll3 = diceRoll[2]; 
-  // sort the array first then check each index 
+  // sort the array
   var sortedDiceRoll = diceRoll.sort(function(a, b){return a - b});
 
   // console.log("The dice roll was: " + diceRoll); 
@@ -198,7 +199,7 @@ function threeDiceCiLo (){
     return; 
   }
 
-  // check to see if a 456 was rolled. 
+  // check to see if a 123 was rolled. 
   if(sortedDiceRoll[0] === 1 && sortedDiceRoll[1] === 2 && sortedDiceRoll[2] === 3){
     // console.log("you rolled a 456");
     axiosMessage(diceRoll1 + " " + diceRoll2 + " " + diceRoll3 +  " - 1 2 3 .. You Lost");
@@ -208,7 +209,7 @@ function threeDiceCiLo (){
   //  The most efficient way is to sort first, then find if any two adjacent elements are equal:
   for (i=0; i<sortedDiceRoll.length; i++){
     if (sortedDiceRoll[i] === sortedDiceRoll[i+1]){
-      // there was a pair that wasnt trips -- need to see which index was not the match. 
+      // there was a pair --- we already checked for triples above 
 
       if(i === 0){
         //then the match was 0 and 1 
@@ -218,6 +219,7 @@ function threeDiceCiLo (){
         return;
 
       }
+      // dont need an else if for i = 2 cause then that means no score on the roll. 
       else if (i === 1){
         //then the mmatch was 1 and 2 
         score = sortedDiceRoll[0]
