96c0bd71f8bdd1030b4b9741a9eac3e4a798dce8
perez6736 <perez6736@gmail.com>
2017-12-04 18:50:14 -0500

    added some logging

--- fkm.js
+++ fkm.js
@@ -3,7 +3,7 @@ var axiosMessage = require('./axiosMessage.js');
 // will randomly send a message saying fkm or fak key to the channel. 
 const FKM = function fkm(){
     var randomchance = Math.floor(Math.random()*1000);
-  
+    console.log(" random number was " + randomchance);
     if(randomchance === 1){
       axiosMessage("Fak Key");
     }
