6b7bee13acdb72e49e28b272bfda75dffd73c2c7
Danny Perez <daniel.perez@structuredweb.com>
2017-12-19 13:00:26 -0500

    changed nudge function name

--- commands/nudge.js
+++ commands/nudge.js
@@ -5,6 +5,7 @@ var nudge = {
     nudgeEveryone: function (){
         // make sure to pass in the groupid here into this function! 
         axios.getUserIds().then(function(fromResolve){
+            // userID is an array of all the userIDs in the group. 
             var userID = fromResolve;
             
         });
