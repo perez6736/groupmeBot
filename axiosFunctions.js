de962096187faac4ee6ebe177ce7dc9da3998f7b
Danny Perez <daniel.perez@structuredweb.com>
2017-12-19 12:51:54 -0500

    changed axios function to grab userID instead.

--- axiosFunctions.js
+++ axiosFunctions.js
@@ -26,22 +26,22 @@ var axiosObj = {
 
     // this will grab the nicknames of the users in the group 
     // pass in the groupid to this function to know what nick names to grab from what group. 
-    getGroupNames: function (){
+    getUserIds: function (){
         // made a promise here cause where i call this function i need it to complete and return the userarray before i can do more stuff. 
         return new Promise(function(resolve, reject){
             axios.get('https://api.groupme.com/v3/groups?token=' + token)
             .then(function (response) {
                 // here is where we grab the names from our group. 
-                var userArray = [];
+                var userIDArray = [];
                 for(i=0; i<response.data.response[0].members.length; i++){
                     
-                    userArray.push(response.data.response[0].members[i].nickname);
+                    userIDArray.push(response.data.response[0].members[i].user_id);
                 }
                 
                 
                 //if we got a list of users we resolve 
-                if(userArray.length>0){
-                    resolve(userArray);
+                if(userIDArray.length>0){
+                    resolve(userIDArray);
                 }
                 //if no list then reject.
                 else{
--- commands/nudge.js
+++ commands/nudge.js
@@ -4,17 +4,9 @@ var axios = require('../axiosFunctions.js');
 var nudge = {
     nudgeEveryone: function (){
         // make sure to pass in the groupid here into this function! 
-        axios.getGroupNames().then(function(fromResolve){
-            var names = fromResolve;
-            console.log(names + " These are the array of names. ");
-            // now we need to include the @ in front of each name. 
-            for(i=0; i<names.length; i++){
-                names[i] = "@"+names[i];
-            }
-            // now i need to seperate the indexes and create one giant string. 
-
-            var atNames = names.join(" ");;
-            axios.postMessage(atNames);
+        axios.getUserIds().then(function(fromResolve){
+            var userID = fromResolve;
+            
         });
     }
 }
