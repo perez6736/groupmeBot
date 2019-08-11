9ddf5bfae161197058d166c89e27623a8afc9d44
perez6736 <perez6736@gmail.com>
2019-08-12 02:53:01 +0900

    updated help

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ var axios = require('../axiosFunctions.js');
 // this will display all the commands the bot knows
 // add the ability to ask for help on certain commands. 
 const helpCommands = function help (){
-  var commands = ["/google", "/help", "/coin", "/gif", "/ciLo"]; 
+  var commands = ["/google", "/help", "/coin", "/gif", "/ciLo", "/cool guy", "/macksmemes"]; 
   axios.postMessage("These are the bot commands: " + commands.join(', '));
 }
 
