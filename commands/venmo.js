46c6f53ec4975314a354ecd1d3ee56074565d1bc
perez6736 <perez6736@gmail.com>
2019-08-12 13:03:56 +0900

    wax

--- commands/venmo.js
+++ commands/venmo.js
@@ -3,7 +3,7 @@ var axios = require('../axiosFunctions.js');
 // function sends a random message form the array. i changed this to max memes but didnt feel like changing the name of the file or anyhting. 
 const venmo = function venmo(){
 
-    axios.postMessage("that's the beauty of venmo you send a  request and I don't have to pay");
+    axios.postMessage("That's the beauty of venmo you send a request but I don't have to pay");
 }
 
 module.exports = venmo;
